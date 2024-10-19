4b4e365d501e259a6ee7d68eda72f29081bb8bd6
bambadiagne <diagne.bambahmadou@gmail.com>
2024-10-19 01:03:04 +0000

    feat: add CORS conf

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Yello Course API')
     .setDescription('The Yello Course API description')
